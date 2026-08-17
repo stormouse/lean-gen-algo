04098ae02ecb63e6df23e79ad99607ff6b68f862
stormouse <stormouse@gmail.com>
2026-08-17 12:55:49 +0800

    reduce confusion

--- Anon4/Grader.lean
+++ Anon4/Grader.lean
@@ -8,8 +8,8 @@ theorem solution_is_optimal' (xs : List Segment) :
     ∀ l r : Nat, l ≤ r →
       let p := (xs.drop l).take (r - l + 1)
       let m := query l r xs
-      (∀ a b c d : Nat, runOperation a b c d p = some ys → score ys ≤ m) ∧
-      (∃ a b c d : Nat, runOperation a b c d p = some zs ∧ score zs = m) := by
+      (∃ ys, ∀ a b c d : Nat, runOperation a b c d p = some ys → score ys ≤ m) ∧
+      (∃ zs, ∃ a b c d : Nat, runOperation a b c d p = some zs ∧ score zs = m) := by
   exact solution_is_optimal xs
 
 #print axioms solution_is_optimal'

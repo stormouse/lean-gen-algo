52ecef6f2cbbb14d05a6a4cd75ccda076278c4f8
stormouse <stormouse@gmail.com>
2026-08-21 18:34:29 +0800

    add some more constraints

--- Anon5/Grader.lean
+++ Anon5/Grader.lean
@@ -3,9 +3,14 @@ import Anon5.Student
 
 namespace Anon5
 
-theorem solution_is_valid' (ms : List Nat) (k : Nat) :
+theorem solution_is_valid'
+    (ms : List Nat)
+    (k : Nat)
+    (hms : ms ≠ [])
+    (hpos : ∀ m ∈ ms, 0 < m)
+    (hk : 0 < k) :
     solution ms k = kthSmallest (multiples ms) k := by
-  exact solution_is_valid ms k
+  exact solution_is_valid ms k hms hpos hk
 
 #print axioms solution_is_valid'
 
--- Anon5/Student.lean
+++ Anon5/Student.lean
@@ -6,7 +6,12 @@ def solution (ms : List Nat) (k : Nat) : Nat :=
   sorry
 
 
-theorem solution_is_valid (ms : List Nat) (k : Nat) :
+theorem solution_is_valid
+    (ms : List Nat)
+    (k : Nat)
+    (hms : ms ≠ [])
+    (hpos : ∀ m ∈ ms, 0 < m)
+    (hk : 0 < k) :
     solution ms k = kthSmallest (multiples ms) k := by
   sorry
 
